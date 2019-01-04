70ef761a4ce6923cefaabe56950b118a1043ed7b
Xian Wang <drew.wang@supernovacompanies.biz>
2019-01-04 15:40:31 +0800

    add basic.py

---
+++ basis.py
@@ -0,0 +1,20 @@
+print('hello python')
+
+strList = ['a', 'b', 'c']
+print(len(strList))
+
+for st in strList:
+    print(st)
+
+print(strList[2])
+
+person = {'name': 'drew.wang', 'age': 20}
+
+print('age:', person['age'])
+
+
+def showInfo():
+    print('name:', 'drew.wang')
+
+
+showInfo()
